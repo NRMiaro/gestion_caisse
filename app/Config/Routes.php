2e93af428e711ae193077555ba1d8b350c57ec88
NRMiaro <miarotools@gmail.com>
2026-06-17 10:35:17 +0300

    page choix de caisse

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('caisse', function($routes){
+    $routes->get('choix', "CaisseController::choix");
+    $routes->post('choix', "CaisseController::validerChoix");
+});
+
+$routes->group('achat', function($routes){
+    $routes->get('saisie', "AchatController::saisie");
+});
